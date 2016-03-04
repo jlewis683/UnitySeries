b82646b8796cf86199497a3d6fb69c1975f0cbc0
Jacob Lewis <jlewis683@gmail.com>
2016-03-03 23:54:06 -0800

    created repo

---
+++ Scripts/Demo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour
+{
+	public int health;
+
+	void Start()
+	{
+		DamagePlayer (5);
+	}
+
+	void DamagePlayer(int damage)
+	{
+		health -= damage;
+	}
+
+	void Update()
+	{
+		if (health <= 0) 
+		{
+			health = 0;
+			Destroy (gameObject, 2f);
+			Debug.Log ("The player has died. Health: " + health);
+		} else if(health >= 15)
+		{
+			print ("You have a lot of health: " + health);
+		}
+	}
+}
